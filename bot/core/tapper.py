d7cad7baf77e4796d51bc3b44edb006ace6a00cd
vsauko <37264286+GravelFire@users.noreply.github.com>
2024-10-03 17:33:50 +0300

    Update tapper.py

--- bot/core/tapper.py
+++ bot/core/tapper.py
@@ -318,8 +318,8 @@ class Tapper:
                         youtube_answers = [
                             {'id': 141, 'answer': 'dildo'},
                             {'id': 146, 'answer': 'dip'},
-                            {'id': 148, 'answer': 'airnode'},
-                            {'id': 149, 'answer': 'wei'},
+                            {'id': 148, 'answer': 'AIRNODE'},
+                            {'id': 149, 'answer': 'WEI'},
                         ]
 
                         if type == 'SUBSCRIBE_TO_CHANNEL':
